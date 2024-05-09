ded9af93dfea80419a141a7ed2b59cbcc5b4ad96
spz2020 <74664513+spz2020@users.noreply.github.com>
2024-05-09 17:55:40 +0800

    install.sh 0.2

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 pkg cl
 pkg update 
-pkg in wget x11-repo -y
-pkg in termux-x11-nightly -y
+pkg in wget cabextract x11-repo -y
+pkg in termux-x11-nightly  -y
 wget https://github.com/spz2020/SpzBox/releases/download/glibc/glibc.tar.xz 
 tar xf glibc.tar.xz -C $PREFIX
 wget https://github.com/spz2020/SpzBox/releases/download/wine/wine-8.21-x86.tar.xz
@@ -30,3 +30,4 @@ export BOX86_NOBANNER=1 WINE=wine WINESERVER=~/wine/bin/wineserver
 wine '"/data/data/com.termux/files/usr/bin/winetricks "'"$@"' > /data/data/com.termux/files/usr/bin/winetricks32
 chmod +x /data/data/com.termux/files/usr/bin/{wine*,regedit}
 echo "安装结束 现在你可以输入wine并按回车检查是否生效"
+rm -rf glibc.tar.xz wine-8.21-x86.tar.xz
